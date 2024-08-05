ced561b6bf5ee3e68adcb371b885b3617bfd59bd
Milica Gareski <milicakiprovska77@gmail.com>
2024-08-05 20:19:04 +0200

    update FAQ section

--- E-shop/src/components/contact/FAQ.tsx
+++ E-shop/src/components/contact/FAQ.tsx
@@ -5,7 +5,11 @@ export default function FAQ() {
   return (
     <section className="container faq">
       <h1 id="faq">
-        Check out our FAQ section—you might find the answers you're looking for!
+        Check out our{" "}
+        <span>
+          <a href="#">FAQ</a>
+        </span>{" "}
+        section—you might find the answers you're looking for!
       </h1>
       <Carousel
         autoPlay={true}
@@ -16,7 +20,9 @@ export default function FAQ() {
       >
         {FAQquestion.map((question) => (
           <div key={question.id}>
-            <h1>{question.question}</h1>
+            <h1>
+              <a href="#">{question.question}</a>
+            </h1>
             <p>{question.answer}</p>
           </div>
         ))}
